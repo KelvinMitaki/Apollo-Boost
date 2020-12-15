c7b62d851de8eb28d3259dcd5d49ca30075447b5
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-15 17:35:54 +0300

    transformed signup to a controlled component

--- client/src/components/Auth/Signup.tsx
+++ client/src/components/Auth/Signup.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
   return (
     <div className="App">
       <h2 className="App">Signup</h2>
       <form className="form">
-        <input type="text" name="username" placeholder="Username" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={e => setUsername(e.target.value)}
+          value={username}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
         <input
           type="password"
           name="confirmPassword"
           placeholder="Confirm Password"
+          onChange={e => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
         />
         <button type="submit" className="button-primary">
           Submit
